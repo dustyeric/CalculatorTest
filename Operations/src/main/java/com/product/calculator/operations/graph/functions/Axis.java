1240a6d330eb96eec835aebc8dc22a243f72e1f9
damolavictor <dav.oduyemi@gmail.com>
2015-07-21 15:53:35 +0100

    added the awt import

--- Operations/src/main/java/com/product/calculator/operations/graph/functions/Axis.java
+++ Operations/src/main/java/com/product/calculator/operations/graph/functions/Axis.java
@@ -1,58 +1,11 @@
 package com.product.calculator.operations.graph.functions;
 
+import java.awt.*;
 import java.util.Vector;
 import java.util.Enumeration;
 
 
-/*
-**************************************************************************
-**
-**    Class  Axis            
-**
-**************************************************************************
-**    Copyright (C) 1995, 1996 Leigh Brookshaw
-**
-**    This program is free software; you can redistribute it and/or modify
-**    it under the terms of the GNU General Public License as published by
-**    the Free Software Foundation; either version 2 of the License, or
-**    (at your option) any later version.
-**
-**    This program is distributed in the hope that it will be useful,
-**    but WITHOUT ANY WARRANTY; without even the implied warranty of
-**    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**    GNU General Public License for more details.
-**
-**    You should have received a copy of the GNU General Public License
-**    along with this program; if not, write to the Free Software
-**    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-**************************************************************************
-**
-**    This class is designed to be used in conjunction with 
-**    the Graph2D class and DataSet class for plotting 2D graphs.
-**
-*************************************************************************/
 
-
-/**
- * This class controls the look and feel of axes.
- * It is designed to be used in conjunction with
- * the Graph2D class and DataSet class for plotting 2D graphs.
- * <p/>
- * To work with the other classes a system of registration is used.
- * The axes have to be attached to the controlling Graph2D class
- * and the DataSet's have to be attached to both the Graph2D class
- * and the Axis class.
- * <p/>
- * This way the 3 main classes Graph2D, Axis and DataSet know of each
- * others existence.
- * <p/>
- * This does not mean the classes cannot be used independently, they can
- * but in this mode nothing is automated, the user must code everything
- * manually
- *
- * @author Leigh Brookshaw
- * @version $Revision: 1.12 $, $Date: 1996/09/12 03:27:07 $.
- */
 
 public class Axis extends Object {
     
